5e83667adf8cc8816d5697c69d79a3e00affac47
russro <pr.russell.ro@gmail.com>
2024-04-26 21:05:38 -0700

    Revert "Add Discord bot functionality."
    This reverts commit 1c4c5279054affce9e11325918f22971b70816f2.

--- main.py
+++ main.py
@@ -1,64 +1,26 @@
 
-import os
 import scrape
 import asyncio
 
-from typing import Final
-from dotenv import load_dotenv
-from discord import Intents, Client, Message
-from responses import get_response
+from typing import List
 
+# TODO: put into JSON file
+URL = r"https://unisat.io/runes/market?tick=DOG%E2%80%A2GO%E2%80%A2TO%E2%80%A2THE%E2%80%A2MOON"
+ELEMENTS_PER_PAGE = 20
+SELECTORS = [
+    f"#rc-tabs-0-panel-1 > div > div.trade-list > div:nth-child({x+1}) > div.content.display-domain.white > div.price-line > span.price"
+    for x in range(ELEMENTS_PER_PAGE)
+]
 
-# LOAD TOKEN
-load_dotenv()
-TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+def call_prices(url: str, selectors: List[str], elements_per_page: int):
+    """Scrapes prices from Unisat URL; updates and returns prices for tokens.
+    """
+    price_elements = scrape.extract_price_elements(url, selectors, elements_per_page) # scrape price data at time
+    updated_entries = scrape.update_db_entries(url, price_elements) # update database
 
-# BOT SETUP
-intents: Intents = Intents.default()
-intents.message_content = True
-client: Client = Client(intents=intents)
-
-
-# MESSAGE FUNCTIONALITY
-async def send_message(message: Message, user_message: str) -> None:
-    if not user_message:
-        print('(Message was empty because intents were not enabled, probably.)')
-        return
-    
-    if is_private := user_message[0] == '!':
-        user_message = user_message[1:]
-
-    try:
-        response: str = await get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
-
-# BOT STARTUP
-@client.event
-async def on_ready() -> None:
-    print(f'{client.user} is now running!')
-
-# HANDLING INCOMING MESSAGES
-@client.event
-async def on_message(message: Message) -> None:
-    if message.author == client.user:
-        return
-    
-    username: str = str(message.author)
-    user_message: str = message.content
-    channel: str = str(message.channel)
-
-    print(f'[{channel}] {username}: "{user_message}"')
-    await send_message(message, user_message)
-
-def main() -> None:
-    asyncio.run(bot.start(token=TOKEN))
+    return updated_entries
 
 
 if __name__ == "__main__":
-
-    main()
-
-    # entries = call_prices(URL, SELECTORS, ELEMENTS_PER_PAGE)
-    # print(entries)
+    entries = call_prices(URL, SELECTORS, ELEMENTS_PER_PAGE)
+    print(entries)
