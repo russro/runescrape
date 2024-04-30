7d508f0a81c4143b968013b2754e4741d67c89ff
russro <pr.russell.ro@gmail.com>
2024-04-29 18:21:20 -0700

    Change func name.

--- main.py
+++ main.py
@@ -10,11 +10,7 @@ from typing import List
 
 # TODO: put into JSON file
 URL = r"https://unisat.io/runes/market?tick=DOG%E2%80%A2GO%E2%80%A2TO%E2%80%A2THE%E2%80%A2MOON"
-ELEMENTS_PER_PAGE = 20
-SELECTORS = [
-    f"#rc-tabs-0-panel-1 > div > div.trade-list > div:nth-child({x+1}) > div.content.display-domain.white > div.price-line > span.price"
-    for x in range(ELEMENTS_PER_PAGE)
-]
+
 
 nest_asyncio.apply()
 
@@ -29,10 +25,10 @@ access_token = os.getenv('DISCORD_TOKEN')
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 
-def call_store_prices(url: str, selectors: List[str], elements_per_page: int):
+def call_save_prices(url: str):
     """Scrapes prices from Unisat URL; updates and returns prices for tokens.
     """
-    price_elements = asyncio.run(scrape.extract_price_elements(url, selectors, elements_per_page)) # scrape price data at time
+    price_elements = asyncio.run(scrape.extract_price_elements(url)) # scrape price data at time
     updated_entries = scrape.update_db_entries(url, price_elements) # update database
 
     return updated_entries
@@ -43,7 +39,7 @@ def entries_to_str(entries):
 
 async def scrape(ctx, url):
     await ctx.send('Scraping...')
-    curr_entries = call_store_prices(url, SELECTORS, ELEMENTS_PER_PAGE)
+    curr_entries = call_save_prices(url)
     await ctx.send(curr_entries)
 
 # @client.event()
